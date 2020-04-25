9c85a2378c38de0b095a648247c9ed24790978ed
st0rmin <54647080+st0rmin@users.noreply.github.com>
2020-04-25 02:33:40 -0500

    Add files via upload
    iterative changes for processing all of the data into excel sheets

--- toDayConvert.py
+++ toDayConvert.py
@@ -1,21 +1,46 @@
 import pandas as pd
-data = pd.read_csv("socialdistancing/2020/01/01/2020-01-01-social-distancing.csv")
-main = pd.read_csv("fips/us-state-ansi-fips.csv")
+month = "3"
+date = "8"
+intmonth = int(month)
+intdate = int(date)
+while(((intmonth == 3) & (intdate <= 31)) | ((intmonth == 4) & (intdate <= 21))):
+    if (intdate < 10):
+        date = "0" + date
+        intdate = int(date)
 
-main['totalCount'] = 0
-main['atHome'] = 0
-main['percentAtHome'] = 0
+    data = pd.read_csv("2020/0" + month + "/" + date + "/2020-0" + month + "-" + date + "-social-distancing.csv")
+    main = pd.read_csv("us-state-ansi-fips.csv")
 
-num = data.shape[0]
-for i in range(num):
-    fipsID = data['origin_census_block_group'][i]
-    fipsID = int(fipsID//1e10)
+    main['totalCount'] = 0
+    main['atHome'] = 0
+    main['percentAtHome'] = 0
 
-    index = main.index[main['st'] == fipsID]
+    num = data.shape[0]
+    print(num)
+    for i in range(num):
+        fipsID = data['origin_census_block_group'][i]
+        fipsID = int(fipsID//1e10)
 
-    deviceCount = data['device_count'].iloc[i] + main.loc[index, 'totalCount']
-    atHome = data['completely_home_device_count'].iloc[i] + main.loc[index, 'atHome']
+        index = main.index[main['st'] == fipsID]
 
-    main.loc[index, 'totalCount'] = deviceCount
-    main.loc[index, 'atHome'] = atHome
-    main.loc[index, 'percentAtHome'] = (atHome/deviceCount)*100
+        deviceCount = data['device_count'].iloc[i] + main.loc[index, 'totalCount']
+        atHome = data['completely_home_device_count'].iloc[i] + main.loc[index, 'atHome']
+
+        main.loc[index, 'totalCount'] = deviceCount
+        main.loc[index, 'atHome'] = atHome
+        main.loc[index, 'percentAtHome'] = (atHome/deviceCount)*100
+
+        if (i % 20000 == 0):
+            print(i / num * 100)
+
+    path = r"dataframes\data" + month + "-" + date + ".xlsx"
+    main.to_excel(path, sheet_name='main', index = False)
+
+    intdate += 1
+    date = str(intdate)
+
+    if (intdate == 31):
+        intmonth += 1
+        month = str(intmonth)
+        intdate = 1
+        date = str(intdate)
