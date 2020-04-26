9530dfe7f49d5bb74f8b73e9a2273c73c6bae295
st0rmin <54647080+st0rmin@users.noreply.github.com>
2020-04-26 11:28:00 -0500

    loop through all csv, convert to excel sheet

--- toDayConvert.py
+++ toDayConvert.py
@@ -1,13 +1,18 @@
 import pandas as pd
+# start dates for data
 month = "3"
 date = "8"
 intmonth = int(month)
 intdate = int(date)
+
+# loop that loops through all of March and up to current April data
 while(((intmonth == 3) & (intdate <= 31)) | ((intmonth == 4) & (intdate <= 21))):
+    # append a 0 if single digit number for file formatting
     if (intdate < 10):
         date = "0" + date
         intdate = int(date)
 
+    # read csv
     data = pd.read_csv("2020/0" + month + "/" + date + "/2020-0" + month + "-" + date + "-social-distancing.csv")
     main = pd.read_csv("us-state-ansi-fips.csv")
 
@@ -15,8 +20,8 @@ while(((intmonth == 3) & (intdate <= 31)) | ((intmonth == 4) & (intdate <= 21)))
     main['atHome'] = 0
     main['percentAtHome'] = 0
 
+    # load data into dataframe
     num = data.shape[0]
-    print(num)
     for i in range(num):
         fipsID = data['origin_census_block_group'][i]
         fipsID = int(fipsID//1e10)
@@ -30,15 +35,19 @@ while(((intmonth == 3) & (intdate <= 31)) | ((intmonth == 4) & (intdate <= 21)))
         main.loc[index, 'atHome'] = atHome
         main.loc[index, 'percentAtHome'] = (atHome/deviceCount)*100
 
+        # rough percentage calculator
         if (i % 20000 == 0):
             print(i / num * 100)
 
+    # put dataframe into easy to use excel sheet
     path = r"dataframes\data" + month + "-" + date + ".xlsx"
     main.to_excel(path, sheet_name='main', index = False)
 
+    # increment day
     intdate += 1
     date = str(intdate)
 
+    # swap months if the last day of March is reached
     if (intdate == 31):
         intmonth += 1
         month = str(intmonth)
